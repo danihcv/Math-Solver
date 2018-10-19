98af1d2fe350987bb22302ce8ddd28baf324092b
Danihcv <dhcv@ic.ufal.br>
2018-10-18 23:09:01 -0300

    Melhorias na exibição das informações; adição do suporta ao operador de divisão; adição do campo Expression.operators

--- Expression.py
+++ Expression.py
@@ -1,20 +1,23 @@
 import re
 
-class Expression:
+regexOperators = r'[-+*^/]'
 
+
+class Expression:
     text = None
+    operators = None
     operands = None
     variables = None
-    numbers = None
 
     def __init__(self, text):
         self.text = text
-        self.operands = re.split(r'-|\+|\*|\^', self.text)
+        self.operators = re.findall(regexOperators, self.text)
+        self.operands = re.split(regexOperators, self.text)
         self.variables = [x for x in self.operands if x.isalpha()]
 
     def __str__(self):
-        string = ""
-        string += self.text + '\n'
-        string += str(self.operands) + '\n'
-        string += str(self.variables) + '\n'
+        string = "\tInput: " + self.text + '\n'
+        string += "\tOperators: " + str(self.operators) + '\n'
+        string += "\tOperands: " + str(self.operands) + '\n'
+        string += "\tVariables: " + str(self.variables) + '\n'
         return string
--- factorization.py
+++ factorization.py
@@ -4,13 +4,16 @@ from Expression import Expression
 # rule1 = ['a','^','2','-','b','^','2']
 # rule2 = ['a','^','2','-','a','b','^','2']
 rules = []
-while (True):
+print("- Rules:")
+while True:
     rule = input()
-    if (rule == "END"): break
+    if rule.upper() == "END":
+        break
     rules += [list(rule)]
 for rule in rules:
     print(rule)
 
+print("\n- Expressions:")
 text = input()
 expression = Expression(text)
 print(expression)
